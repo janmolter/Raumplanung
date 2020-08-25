e4da15cdf1c7fd60706f301c53044b87dfa7694b
janmolter <66870028+janmolter@users.noreply.github.com>
2020-08-25 16:30:22 +0000

    update settings.py für merge

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'crispy_forms',
     'room',
     'django_filters',
     'bootstrapform'
